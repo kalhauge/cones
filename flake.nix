05dbffbe42aefa38ba3c0a850b129fd067c665c5
Christian Gram Kalhauge <chrg@dtu.dk>
2024-07-24 08:43:51 +0200

    Add haskellOverlay

--- flake.nix
+++ flake.nix
@@ -1,66 +1,64 @@
 {
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
-    flake-utils.url = github:numtide/flake-utils;
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    , ...
-    }@inputs:
-    let
-      packages = p: {
-        "cones" = p.callCabal2nixWithOptions "cones" "${self}/cones" "" { };
-        "conedec" = p.callCabal2nixWithOptions "conedec" "${self}/conedec" "" { };
-      };
-      overlays = final: prev: {
-        haskellPackages = prev.haskellPackages.extend (p: _: packages p);
-      };
-    in
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ inputs: let
+    haskellOverlay = pkgs: final: prev: {
+      "cones" = final.callCabal2nixWithOptions "cones" "${self}/cones" "" {};
+      "conedec" = final.callCabal2nixWithOptions "conedec" "${self}/conedec" "" {};
+    };
+    overlays = final: prev: {
+      haskellPackages = prev.haskellPackages.extend (haskellOverlay final);
+    };
+  in
     {
+      inherit haskellOverlay;
       overlays.default = overlays;
-    } //
-    flake-utils.lib.eachDefaultSystem
-      (system:
-      let
-        hpkgs = (import nixpkgs {
+    }
+    // flake-utils.lib.eachDefaultSystem
+    (system: let
+      hpkgs =
+        (import nixpkgs {
           inherit system;
-          overlays = [ overlays ];
-        }).haskellPackages;
-      in
-      rec {
-        packages = {
-          default = hpkgs.cones;
-          cones = hpkgs.cones;
-          conedec = hpkgs.conedec;
-        };
-        devShells =
-          let
-            buildInputs = with hpkgs; [
-              cabal-install
-              ghcid
-              haskell-language-server
-              hpack
-              fourmolu
-            ];
-            withHoogle = true;
-          in
+          overlays = [overlays];
+        })
+        .haskellPackages;
+    in {
+      packages = {
+        default = hpkgs.cones;
+        cones = hpkgs.cones;
+        conedec = hpkgs.conedec;
+      };
+      devShells = let
+        buildInputs = with hpkgs; [
+          cabal-install
+          ghcid
+          haskell-language-server
+          hpack
+          fourmolu
+        ];
+        withHoogle = true;
+      in {
+        default =
+          hpkgs.shellFor
           {
-            default = hpkgs.shellFor
-              {
-                name = "cones-shell";
-                packages = p:
-                  [ p.cones p.conedec ];
-                inherit buildInputs withHoogle;
-              };
-            conedec = hpkgs.shellFor
-              {
-                name = "conedec-shell";
-                packages = p:
-                  [ p.conedec ];
-                inherit buildInputs withHoogle;
-              };
+            name = "cones-shell";
+            packages = p: [p.cones p.conedec];
+            inherit buildInputs withHoogle;
           };
-      });
+        conedec =
+          hpkgs.shellFor
+          {
+            name = "conedec-shell";
+            packages = p: [p.conedec];
+            inherit buildInputs withHoogle;
+          };
+      };
+    });
 }
